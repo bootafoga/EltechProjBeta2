89e2acb9bafbda741ecefc145a3916be23ab0916
Мария <m.juice@icloud.com>
2018-10-19 21:57:07 +0300

    Чек

--- app/src/main/java/com/example/max/EltechProj1/Schedule.java
+++ app/src/main/java/com/example/max/EltechProj1/Schedule.java
@@ -20,7 +20,6 @@ public class Schedule extends MainActivity {
     private static final int LAYOUT = R.layout.activity_schedule;
     private TabsPagerFragmentAdapter adapter;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
